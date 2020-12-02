0bfe4ce65762c6b16884dc00ad772f8c06c0cd9f
FebVeg <feb19958@gmail.com>
2020-12-02 14:53:25 +0100

    Update Get-WinWifiPassword.py

--- Get-WinWifiPassword.py
+++ Get-WinWifiPassword.py
@@ -1,10 +1,23 @@
-from os import popen
-from re import search
-from prettytable import PrettyTable
+from tabulate       import tabulate
+from os             import popen
+from re             import search
 
-def wifi_stealer():
+def print_in_a_frame(words, title, borderchar = '#'):
+    size = max(len(word) for word in words)
+    print(borderchar * (size + 4))
+    print('{bc} {:^{}} {bc}'.format(title, size, bc = borderchar))
+    for word in words:
+        print('{bc} {:<{}} {bc}'.format(word, size, bc = borderchar))
+    print(borderchar * (size + 4))
+
+def frame(item, title):
+    lines = item.splitlines()
+    print_in_a_frame(lines, title)
+
+def wifi():
     try:
-        table = PrettyTable(['SSID', 'Passwords'])
+        table = []
+        headers = ['SSID', "Password"]
         job = popen("netsh wlan show profiles").readlines()
         for x in job:
             x = x.strip("\n")
@@ -17,12 +30,11 @@ def wifi_stealer():
                     if "Conten" in a:
                         a = a.partition(": ")
                         a = a[2].strip()
-                        table.add_row([x, a])
-        table.align = "l"
-        print(table)
+                        table.append([x, a])
+        frame(str(tabulate(table, headers, tablefmt="grid")), "WiFi's Profiles")
     except Exception as Error:
         print(str(Error))
         return
 
 if __name__ == "__main__":
-    wifi_stealer() 
+    wifi()
